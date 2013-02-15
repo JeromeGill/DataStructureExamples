6a5f93003f9f210f5bdc7ff8cb24fb745bee7431
Jerome Gill <jerome2.gill@gmail.com>
2013-02-15 17:23:31 +0000

    Initial Commit

---
+++ DataStructures/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DataStructures
+//
+//  Created by Jerome on 15/02/2013.
+//  Copyright (c) 2013 Jerome. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
