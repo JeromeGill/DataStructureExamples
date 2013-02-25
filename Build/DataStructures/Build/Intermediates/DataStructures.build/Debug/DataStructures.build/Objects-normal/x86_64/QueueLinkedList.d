3a60174511b39604b16f63cf76855828b0f4ca94
Jerome Gill <jerome2.gill@gmail.com>
2013-02-25 12:37:51 +0000

    Set  paths relative to project

---
+++ Build/DataStructures/Build/Intermediates/DataStructures.build/Debug/DataStructures.build/Objects-normal/x86_64/QueueLinkedList.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/jerome/Documents/Work/Software/Learning/DataStructures/DataStructures/../../../../../../../Jerome/Documents/Work/Software/Learning/DataStructures/DataStructures/QueueLinkedList.cpp \
+  /Users/jerome/Documents/Work/Software/Learning/DataStructures/DataStructures/../../../../../../../Jerome/Documents/Work/Software/Learning/DataStructures/DataStructures/QueueLinkedList.h \
+  /Users/jerome/Documents/Work/Software/Learning/DataStructures/DataStructures/../../../../../../../Jerome/Documents/Work/Software/Learning/DataStructures/DataStructures/LinkedListExample.h
---
+++ Build/DataStructures/Build/Intermediates/DataStructures.build/Debug/DataStructures.build/Objects-normal/x86_64/StackLinkedList.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/jerome/Documents/Work/Software/Learning/DataStructures/DataStructures/../../../../../../../Jerome/Documents/Work/Software/Learning/DataStructures/DataStructures/StackLinkedList.cpp \
+  /Users/jerome/Documents/Work/Software/Learning/DataStructures/DataStructures/../../../../../../../Jerome/Documents/Work/Software/Learning/DataStructures/DataStructures/StackLinkedList.h \
+  /Users/jerome/Documents/Work/Software/Learning/DataStructures/DataStructures/../../../../../../../Jerome/Documents/Work/Software/Learning/DataStructures/DataStructures/LinkedListExample.h
